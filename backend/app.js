d89898d2717eb529a8a53d041bc9c0cdeb89c9ef
laouinis <laouinis@gmail.com>
2023-02-17 21:48:02 +0100

    typo error fixed

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,20 @@ const sauceRouter = require('./routes/sauces');
 
 mongoose.connect('mongodb+srv://P6-Piicante:P6-Piicante@cluster0.kjvux2p.mongodb.net/?retryWrites=true&w=majority',
     {
-      userNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log('Conneection to MongoDb success !'))
-    .catch(() => console.log('Connexion to MongoDB failed !'));
+    .then(() => console.log('Connection to MongoDb success !'))
+    .catch(() => console.log('Connection to MongoDB failed !'));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-AllowOrigin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    'Access-Control-Allow-Origin',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
   );
   res.setHeader(
-    'Access-Control-Allow-Methods', 'PUT,POST,PUT,DELETE,PATCH, OPTIONS'
+      'Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH, OPTIONS',
   );
   next();
 });
